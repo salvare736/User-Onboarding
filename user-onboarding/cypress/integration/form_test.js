916d264d667f644cbc9e1f332c1c18a7836a90f7
Sergio Alvarez <salvare736@gmail.com>
2021-03-25 16:48:03 -0400

    setup the first part of the MVP test; still not working as intended

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,17 +1,21 @@
-// write tests here
 describe("Form app", () => {
   beforeEach(() => {
-    // arbitrary code you want running before your tests start
     cy.visit("http://localhost:3000/");
   });
 
+  const nameInput = () => cy.get('input[name="name"]');
+  const emailInput = () => cy.get('input[name="email"]');
+  const passwordInput = () => cy.get('input[name="password"]');
+
   it("sanity test to make sure that tests work", () => {
-    // false positive
-    // 'expect' is an assertion
-    // there can be many assertions per test
-    // though inside the it statement (the test itself) usually
-    // those assertions are logically grouped together
     expect(1 + 2).to.equal(3);
     expect(2 + 2).not.to.equal(5);
+  });
+
+  it("fill out the text input fields", () => {
+    nameInput().type("Cynthia Harrell");
+    nameInput().should("have.value", "Cynthia Harrell");
+    emailInput().type("ch@gmail.com");
+    passwordInput().type("snake9001eater");
   });
 });
